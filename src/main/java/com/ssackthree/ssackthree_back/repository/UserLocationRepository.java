ba257c13ac84dab1f30c427d50b279ac43a176d5
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-05-25 21:41:34 +0900

    fix : 홈페이지 메뉴 조회_거리기준 에러 해결

--- src/main/java/com/ssackthree/ssackthree_back/repository/UserLocationRepository.java
+++ src/main/java/com/ssackthree/ssackthree_back/repository/UserLocationRepository.java
@@ -4,6 +4,9 @@ import com.ssackthree.ssackthree_back.entity.UserLocationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserLocationRepository extends JpaRepository<UserLocationEntity, Long> {
+    Optional<UserLocationEntity> findByUserEntityId(long id);
 }
