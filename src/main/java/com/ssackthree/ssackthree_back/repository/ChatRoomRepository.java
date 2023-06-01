5799e65a07445620ccc3d34fe7dc8959f7db2cd5
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-06-01 14:38:17 +0900

    feat: 채팅방 만들 때 이미 두 사용자 사이 채팅방 있으면 만들어 있던 거 리턴, 없으면 새 채팅방 생성 후 리턴

--- src/main/java/com/ssackthree/ssackthree_back/repository/ChatRoomRepository.java
+++ src/main/java/com/ssackthree/ssackthree_back/repository/ChatRoomRepository.java
@@ -2,8 +2,16 @@ package com.ssackthree.ssackthree_back.repository;
 
 import com.ssackthree.ssackthree_back.entity.ChatRoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, Long> {
+
+
+    @Query("SELECT cr FROM ChatRoomEntity cr WHERE (cr.userEntity1.id = :id1 AND cr.userEntity2.id = :id2) OR (cr.userEntity1.id = :id2 AND cr.userEntity2.id = :id1)")
+    Optional<ChatRoomEntity> findByUserEntity1IdAndUserEntity2Id(@Param("id1") long id1, @Param("id2") long id2);
 }
--- src/main/java/com/ssackthree/ssackthree_back/service/ChatService.java
+++ src/main/java/com/ssackthree/ssackthree_back/service/ChatService.java
@@ -6,6 +6,7 @@ import com.ssackthree.ssackthree_back.dto.ChatRoomRequestDto;
 import com.ssackthree.ssackthree_back.dto.ChatRoomResponseDto;
 import com.ssackthree.ssackthree_back.entity.ChatMessageEntity;
 import com.ssackthree.ssackthree_back.entity.ChatRoomEntity;
+import com.ssackthree.ssackthree_back.entity.UserEntity;
 import com.ssackthree.ssackthree_back.repository.ChatMessageRepository;
 import com.ssackthree.ssackthree_back.repository.ChatRoomRepository;
 import com.ssackthree.ssackthree_back.repository.UserRepository;
@@ -28,15 +29,27 @@ public class ChatService {
     private final UserRepository userRepository;
     private final ChatMessageRepository chatMessageRepository;
     private final ObjectMapper objectMapper;
+    static ChatRoomEntity chatRoomEntity;
 
     public ChatRoomResponseDto createChatRoom(ChatRoomRequestDto chatRoomDto){
-        ChatRoomEntity chatRoomEntity = ChatRoomEntity.builder()
-                .userEntity1(userRepository.findById(chatRoomDto.getUserId1()).get())
-                .userEntity2(userRepository.findById(chatRoomDto.getUserId2()).get())
-                .createdDate(LocalDateTime.now())
-                .build();
 
-        chatRoomRepository.save(chatRoomEntity);
+
+        UserEntity user1 = userRepository.findById(chatRoomDto.getUserId1()).get();
+        UserEntity user2 = userRepository.findById(chatRoomDto.getUserId2()).get();
+
+        // 이미 둘 사이에 채팅 방이 존재
+        if(chatRoomRepository.findByUserEntity1IdAndUserEntity2Id(user1.getId(), user2.getId()).isPresent()){
+            chatRoomEntity = chatRoomRepository.findByUserEntity1IdAndUserEntity2Id(user1.getId(), user2.getId()).get();
+        }else{ // 둘 사이에 채팅 방이 없음
+            chatRoomEntity = ChatRoomEntity.builder()
+                    .userEntity1(user1)
+                    .userEntity2(user2)
+                    .createdDate(LocalDateTime.now())
+                    .build();
+
+            chatRoomRepository.save(chatRoomEntity);
+        }
+
 
         ChatRoomResponseDto chatRoomResponseDto = ChatRoomResponseDto.builder()
                 .roomId(chatRoomEntity.getId())
